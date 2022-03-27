a5befe6a263a7181c676596b4d9e8f9859579c86
Jash <jashshah3005@gmail.com>
2022-03-27 06:29:02 +0530

    activity data template and scrollability added

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 import tkinter as tk
 from tkinter import *
 from tkinter import messagebox
+from tkinter import ttk
 import sys
 sys.path.append("")
 app = Tk()
@@ -78,8 +79,8 @@ homeLabel.pack(side="right")
 
 #Menu Click
 def menuClick():
-    switch()
     removeAllTemporary(app)
+    switch()
 # Navbar button:
 navbarBtn = tk.Button(topFrame, image=navIcon, bg=color["purple"], activebackground=color["purple"], bd=0, padx=20, command=menuClick)
 navbarBtn.place(x=10, y=10)
@@ -94,18 +95,79 @@ y = 80
 
 def db_click():
     # messagebox.showinfo("Dashboard Popup")
-    removeAllTemporary(app)
     switch()
+    removeAllTemporary(app)
     dashBoardLabel = Label(app,text='DASHBOARD',bg="white",font = ('bold 15 underline')).place(x=240,y=60)
     lv.append(dashBoardLabel)
 
 
 def SA_click():
-    messagebox.showinfo("Sleep Analytics Popup")
+    # messagebox.showinfo("Sleep Analytics Popup")
+    switch()
+    removeAllTemporary(app)
+    app.resizable(width = 1, height = 1)
+    
+    # Using treeview widget
+    treev = ttk.Treeview(app, selectmode ='browse')
+    
+    # Calling pack method w.r.to treeview
+    treev.place(x=80,y=80)
+    
+    # Constructing vertical scrollbar
+    # with treeview
+    verscrlbar = ttk.Scrollbar(app,
+                               orient ="vertical",
+                               command = treev.yview)
+    
+   
+    
+    # Defining number of columns
+    treev["columns"] = ("1", "2", "3")
+    
+    # Defining heading
+    treev['show'] = 'headings'
+    
+    # Assigning the width and anchor to  the
+    # respective columns
+    treev.column("1", width = 100, anchor ='c')
+    treev.column("2", width = 100, anchor ='c')
+    treev.column("3", width = 100, anchor ='c')
+
+   
+    # respective columns
+    treev.heading("1", text ="Date")
+    treev.heading("2", text ="Time of Sleep")
+    treev.heading("3", text ="Perfect sleep(%)")
+    # treev.insert("", 'end', text ="L1",
+    #              values =("Nidhi", "F", "25"))
+    # treev.insert("", 'end', text ="L2",
+    #              values =("Nisha", "F", "23"))
+    # treev.insert("", 'end', text ="L3",
+    #              values =("Preeti", "F", "27"))
+    # treev.insert("", 'end', text ="L4",
+    #              values =("Rahul", "M", "20"))
+    # treev.insert("", 'end', text ="L5",
+    #              values =("Sonu", "F", "18"))
+    # treev.insert("", 'end', text ="L6",
+    #              values =("Rohit", "M", "19"))
+    # treev.insert("", 'end', text ="L7",
+    #              values =("Geeta", "F", "25"))
+    # treev.insert("", 'end', text ="L8",
+    #              values =("Ankit", "M", "22"))
+    # treev.insert("", 'end', text ="L10",
+    #              values =("Mukul", "F", "25"))
+    # treev.insert("", 'end', text ="L11",
+    #              values =("Mohit", "M", "16"))
+    # treev.insert("", 'end', text ="L12",
+    #              values =("Vivek", "M", "22"))
+    # treev.insert("", 'end', text ="L13",
+    #              values =("Suman", "F", "30"))
+        
+
 def Track_click():
+    switch()
     removeAllTemporary(app)
     # import sleep_tracker
-    switch()
     trackBtn = tk.Button(app,text="Start Tracking", font="BahnschriftLight 15",fg='black',bg="purple",bd=0,activebackground="green",activeforeground="white").place(x=20, y=120)
     # dashBoardLabel.destroy()
     att = tk.Frame(app,bg='white',highlightbackground= "red",highlightcolor= "red",highlightthickness=2,height=300,width=350).place(x=200,y=80)
@@ -132,15 +194,12 @@ def Track_click():
                    
   
     
-ActivityBtn = tk.Button(navapp,text = "Dashboard", font="BahnschriftLight 15 underline", bg="white", fg=color["purple"], activeforeground="green", bd=0,command = db_click).place(x=25, y=y)
+DashboardBtn = tk.Button(navapp,text = "Dashboard", font="BahnschriftLight 15 underline", bg="white", fg=color["purple"], activeforeground="green", bd=0,command = db_click).place(x=25, y=y)
 y += 60
-ActivityBtn = tk.Button(navapp,text = "Sleep Analytics", font="BahnschriftLight 15 underline", bg="white", fg=color["purple"], activeforeground="green", bd=0,command = Track_click).place(x=25, y=y)
+ActivityBtn = tk.Button(navapp,text = "Sleep Analytics", font="BahnschriftLight 15 underline", bg="white", fg=color["purple"], activeforeground="green", bd=0,command = SA_click).place(x=25, y=y)
 y += 60
-ActivityBtn = tk.Button(navapp,text = "Tracking", font="BahnschriftLight 15 underline", bg="white", fg=color["purple"], activeforeground="green", bd=0,command = Track_click).place(x=25, y=y)
+TrackBtn = tk.Button(navapp,text = "Tracking", font="BahnschriftLight 15 underline", bg="white", fg=color["purple"], activeforeground="green", bd=0,command = Track_click).place(x=25, y=y)
 y += 60
-# ActivityBtn = tk.Button(navapp,text = "Tracking", font="BahnschriftLight 15 underline", bg="white", fg=color["purple"], activeforeground="green", bd=0,command = db_click).place(x=25, y=y)
-# y += 60
-# Track_click()
 
 # Navbar Close Button:
 closeBtn = tk.Button(navapp,image=closeIcon, bg=color["purple"], activebackground=color["purple"], bd=0, command=switch)
